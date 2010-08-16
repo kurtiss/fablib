c0036adf4cf175bac94f54d9cd544790cce32117
Kurtiss Hare <kurtiss@gmail.com>
2010-08-15 19:52:28 -0700

    First commit.

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+setup.py
+
+Created by Kurtiss Hare on 2010-08-15.
+Copyright (c) 2010 Medium Entertainment, Inc. All rights reserved.
+"""
+
+from setuptools import setup, find_packages
+import os
+
+os.chdir("src")
+
+execfile(os.path.join('fablib', 'version.py'))
+
+setup(
+    name = 'fablib',
+    version = VERSION,
+    description = 'fablib makes available a few primitives of use to fabric control layers.',
+    author = 'Kurtiss Hare',
+    author_email = 'kurtiss@gmail.com',
+    url = 'http://www.github.com/kurtiss/fablib',
+    packages = find_packages(),
+    scripts = [],
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
---
+++ src/fablib/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+__init__.py
+
+Created by Kurtiss Hare on 2010-08-15.
+Copyright (c) 2010 Medium Entertainment, Inc. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
